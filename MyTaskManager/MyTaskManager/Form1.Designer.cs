9bfb90d9c13d177bc4ea4646740971b9645e46cd
Dmytro <dmutroivd@gmail.com>
2020-11-15 17:05:06 +0200

    Add basic functionality of Task manager(take down process and refreshing list with proceses).

--- MyTaskManager/MyTaskManager/Form1.Designer.cs
+++ MyTaskManager/MyTaskManager/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MyTaskManager
 {
-    partial class Form1
+    partial class MyTaskManager
     {
         /// <summary>
         /// Required designer variable.
@@ -29,12 +29,100 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.ProcessId = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ProcessPriority = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ProcessName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ProcessMemoryUsage = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ProcessThreads = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.AllowColumnReorder = true;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ProcessId,
+            this.ProcessName,
+            this.ProcessPriority,
+            this.ProcessMemoryUsage,
+            this.ProcessThreads});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 23);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(800, 427);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ProcessId
+            // 
+            this.ProcessId.Text = "Id";
+            this.ProcessId.Width = 45;
+            // 
+            // ProcessPriority
+            // 
+            this.ProcessPriority.DisplayIndex = 1;
+            this.ProcessPriority.Text = "Priority";
+            this.ProcessPriority.Width = 90;
+            // 
+            // ProcessName
+            // 
+            this.ProcessName.DisplayIndex = 2;
+            this.ProcessName.Text = "Name";
+            this.ProcessName.Width = 82;
+            // 
+            // ProcessMemoryUsage
+            // 
+            this.ProcessMemoryUsage.Text = "Process memory usage";
+            this.ProcessMemoryUsage.Width = 173;
+            // 
+            // ProcessThreads
+            // 
+            this.ProcessThreads.Text = "Threads";
+            this.ProcessThreads.Width = 84;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(800, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Take down selected process";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MyTaskManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Name = "MyTaskManager";
+            this.Text = "Task manager";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader ProcessId;
+        private System.Windows.Forms.ColumnHeader ProcessPriority;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ColumnHeader ProcessName;
+        private System.Windows.Forms.ColumnHeader ProcessMemoryUsage;
+        private System.Windows.Forms.ColumnHeader ProcessThreads;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- MyTaskManager/MyTaskManager/Form1.cs
+++ MyTaskManager/MyTaskManager/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,85 @@ using System.Windows.Forms;
 
 namespace MyTaskManager
 {
-    public partial class Form1 : Form
+    public partial class MyTaskManager : Form
     {
-        public Form1()
+        public MyTaskManager()
         {
             InitializeComponent();
+            Init();
+            timer1.Start();
+
+        }
+        private void Init()
+        {
+            foreach (var p in Process.GetProcesses())
+            {
+                listView1.Items.Add(GetListViewItem(p));
+            }
+        }
+        private ListViewItem GetListViewItem(Process process)
+        {
+
+            ListViewItem lv = new ListViewItem();
+            lv.Tag = process;
+            lv.Text = process.Id.ToString();
+
+            lv.SubItems.Add(new ListViewItem.ListViewSubItem(lv, process.ProcessName));
+            lv.SubItems.Add(new ListViewItem.ListViewSubItem(lv, process.BasePriority.ToString()));
+            lv.SubItems.Add(new ListViewItem.ListViewSubItem(lv, process.WorkingSet64.ToString()));
+            lv.SubItems.Add(new ListViewItem.ListViewSubItem(lv, process.Threads.Count.ToString()));
+
+
+
+            return lv;
+        }
+        private void RefreshListView()
+        {
+            //Add new 
+            var writtenProceses = listView1.Items.Cast<ListViewItem>().Select(i => (Process)i.Tag).ToList();
+            var currentProceses = Process.GetProcesses().ToList();
+            foreach (var p in currentProceses)
+            {
+                //Add new
+                if (!writtenProceses.Exists(pr => pr.Id == p.Id))
+                {
+                    listView1.Items.Add(GetListViewItem(p));
+                }
+
+
+            }
+
+
+
+            foreach (var item in listView1.Items.Cast<ListViewItem>())
+            {
+                if (!currentProceses.Exists(p => p.Id == ((Process)item.Tag).Id))
+                {
+                    listView1.Items.Remove(item);
+                }
+            }
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            RefreshListView();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var ToTakeDownProcess = listView1.SelectedItems.Cast<ListViewItem>().Select(x => (Process)x.Tag);
+            foreach (var proc in ToTakeDownProcess)
+            {
+                try
+                {
+                    proc.Kill();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("You cannot kill this process");
+                }
+            }
+            RefreshListView();
         }
     }
 }
